16db5c45ace9852dee3e1cfb43ecc9b00a7515f6
Christophe Donnat <chdonnat@student.42perpignan.fr>
2025-03-19 11:06:01 +0100

    ADDED miniRT.h

---
+++ includes/miniRT.h
@@ -0,0 +1,15 @@
+#ifndef MINIRT_H
+# define MINIRT_H
+
+// libraries
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <math.h>
+
+// headers
+# include "../libft/includes/libft.h"
+# include "../minlibx-linux/mlx.h"
+
+#endif
